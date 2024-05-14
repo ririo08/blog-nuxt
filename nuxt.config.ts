b44f64cfd1e1cbe45e219bdd5719382fd9023843
ririo08 <ririo08@users.noreply.github.com>
2024-05-15 02:11:31 +0900

    chore: add modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible',
     // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
+    '@nuxt/devtools',
+    '@nuxthq/studio'
   ]
 })
